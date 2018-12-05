a34dbc4fe8621308827efb7dabe369489b21e7c2
Gaurang Sadekar <gaurang.sadekar@gmail.com>
2018-12-04 23:16:43 -0500

    day 1 done

--- src/main/java/aoc2018/Day1.java
+++ src/main/java/aoc2018/Day1.java
@@ -1,9 +1,11 @@
 package aoc2018;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Day1 {
-    public long solution(List<String> frequencies) {
+    public long solution1(List<String> frequencies) {
         long result = 0;
         for (String freq : frequencies) {
             long value = Integer.parseInt(freq.substring(1));
@@ -15,6 +17,28 @@ public class Day1 {
                     result -= value;
                     break;
             }
+        }
+        return result;
+    }
+
+    public long solution2(List<String> frequencies) {
+        Set<Long> hitFreqs = new HashSet<>();
+        long result = 0;
+
+        int i = 0;
+        while (!hitFreqs.contains(result)) {
+            hitFreqs.add(result);
+            String freq = frequencies.get(i);
+            long value = Integer.parseInt(freq.substring(1));
+            switch (freq.charAt(0)) {
+                case '+':
+                    result += value;
+                    break;
+                case '-':
+                    result -= value;
+                    break;
+            }
+            i = (i + 1) % frequencies.size();
         }
 
         return result;
