1fa209c530a7cc6acd90c75e9870ce7a7360a60a
gaurangsadekar <gaurang.sadekar@gmail.com>
2018-12-07 12:03:32 -0500

    day 3 part 1

--- src/main/java/aoc2018/Day3.java
+++ src/main/java/aoc2018/Day3.java
@@ -1,11 +1,8 @@
 package aoc2018;
 
-import javax.sound.midi.SysexMessage;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class Day3 {
 
@@ -25,13 +22,34 @@ public class Day3 {
         return null;
     }
 
+    private static int[][] grid = new int[1000][1000];
+    public static void fillGrid(Claim claim) {
+        int xBound = claim.x + claim.width;
+        int yBound = claim.y + claim.height;
+
+        for (int i = claim.x; i < xBound; i++)
+            for (int j = claim.y; j < yBound; j++)
+                grid[i][j]++;
+    }
+
     public int solution1(List<String> claimStrs) {
-        List<Claim> claims = claimStrs
+        claimStrs
                 .stream()
                 .map(Day3::processClaim)
-                .collect(Collectors.toList());
+                .forEach(Day3::fillGrid);
 
-        
+        int overlappingSquares = 0;
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid.length; j++)
+                if (grid[i][j] > 1) overlappingSquares++;
+
+        return overlappingSquares;
+    }
+
+    public int solution2(List<String> claimStrs) {
+        claimStrs
+                .stream()
+                .map(Day3::processClaim);
 
         return 0;
     }
